e3080a5fb9d21576a5dea14f0e15f6c1eb00c80d
Tom Bryant <tomcbryant@gmail.com>
2015-06-30 08:19:57 +0100

    Move debug mouse handling to debug class

--- src/Prototype4/Board.java
+++ src/Prototype4/Board.java
@@ -25,6 +25,7 @@ public class Board extends PApplet {
         this.center=new HexTile(this, SCREEN_WIDTH/2, SCREEN_HEIGHT/2,50, init,init.getResourceTiles()[0],init.getTokens()[0]);
         center.getModel().buildBoard((center));
         this.debugger = new Debug(this,center);
+        textSize(14);
         background(0, 188, 212);
         debugger.displayClosed();
 
@@ -34,51 +35,7 @@ public class Board extends PApplet {
 
     }
 
-    public void backgroundAndRandButton(){
-        stroke(0,0,0,0);
-        textSize(14);
-        int randButtonX = (int) (SCREEN_WIDTH - 40 - textWidth("   Random   "));
-        int randButtonY = 10;
-        int classicButtonX = (int) (randButtonX - 10 - textWidth("   Classic   "));
-        int classicButtonY = 10;
-        int dPButtonX =(int) (classicButtonX - 10 - textWidth("   Debug Points   "));
-        int dPButtonY = 10;
-        int dSButtonX = (int) (dPButtonX - 10 - textWidth("   Debug Sides   "));
-        int dSButtonY = 10 ;
-        int dRButtonX = (int) (dSButtonX - 10 - textWidth("   Debug Resources   "));
-        int dRButtonY = 10 ;
-        int noDebugButtonX = (int) (dRButtonX - 10 - textWidth("   Debug Off   "));
-        int noDebugButtonY = 10 ;
 
-
-        smooth(8);
-        fill(0);
-        text("Prototype 3.3", 20,SCREEN_HEIGHT - 20);
-        fill(244,67,54);
-        rect(randButtonX,randButtonY , textWidth("   Random   "), 30);
-        fill(0);
-        text("   Random   ", randButtonX, 30 );
-        fill(76,175,80);
-        rect(classicButtonX,classicButtonY , textWidth("   Classic   "), 30);
-        fill(0);
-        text("   Classic   ", classicButtonX, 30 );
-        fill(255,243,224);
-        rect(dPButtonX,dPButtonY , textWidth("   Debug Points   "), 30);
-        fill(0);
-        text("   Debug Points   ", dPButtonX, 30 );
-        fill(255,243,224);
-        rect(dSButtonX,dSButtonY , textWidth("   Debug Sides   "), 30);
-        fill(0);
-        text("   Debug Sides   ", dSButtonX, 30 );
-        fill(255,243,224);
-        rect(dRButtonX, dRButtonY , textWidth("   Debug Resources   "), 30);
-        fill(0);
-        text("   Debug Resources   ", dRButtonX, 30 );
-        fill(255,243,224);
-        rect(noDebugButtonX,noDebugButtonY , textWidth("   Debug Off   "), 30);
-        fill(0);
-        text("   Debug Off   ", noDebugButtonX, 30 );
-    }
 
 
 
@@ -98,8 +55,6 @@ public class Board extends PApplet {
             debugger.displayClosed();
             center.getModel().displayBoard();
         }
-        //System.out.println(center.checkNeighbor("AB"));
-
 
         fill(0);
 
@@ -121,63 +76,7 @@ public class Board extends PApplet {
 
     @Override
     public void mousePressed() {
-        if(!debugger.open) {
-            if(debugger.overHead()){
-                debugger.open = true;
-            } else {
-                debugger.open = false;
-            }
-        }
-            if(debugger.open){
-                int result = debugger.overMenu();
-                if(result == 1) {
-                    clear();
-                    background(0, 188, 212);
-                    debugger.displayClosed();
-                    debugger.open = false;
-                    BoardData reInit = new BoardData();
-                    this.center = new HexTile(this, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, reInit, reInit.getResourceTiles()[0], reInit.getTokens()[0]);
-                    this.center.getModel().buildRandomBoard((center));
-                }
-                if(result == 2){
-                    clear();
-                    background(0, 188, 212);
-                    debugger.displayClosed();
-                    debugger.open = false;
-                    backgroundAndRandButton();
-                    BoardData reInit = new BoardData();
-                    this.center = new HexTile(this, SCREEN_WIDTH/2, SCREEN_HEIGHT/2,50, reInit,reInit.getResourceTiles()[0],reInit.getTokens()[0]);
-                    this.center.getModel().buildBoard(center);
-                }
-                if(result == 3){
-                    background(0, 188, 212);
-                    debugger.displayClosed();
-                    debugger.open = false;
-                    center.getModel().displayBoard();
-                    center.getModel().setDisplayMode(1);
-                }
-                if(result == 4){
-                    background(0, 188, 212);
-                    debugger.displayClosed();
-                    debugger.open = false;
-                    center.getModel().displayBoard();
-                    center.getModel().setDisplayMode(2);
-                }
-                if(result == 5){
-                    background(0, 188, 212);
-                    debugger.displayClosed();
-                    debugger.open = false;
-                    center.getModel().displayBoard();
-                    center.getModel().setDisplayMode(3);
-                }
-                if(result == 6){
-                    background(0, 188, 212);
-                    debugger.displayClosed();
-                    debugger.open = false;
-                    center.getModel().displayBoard();
-                    center.getModel().setDisplayMode(0);
-                }
-            }
+        debugger.mouseDebug();
 
     }
 
--- src/Prototype4/Debug.java
+++ src/Prototype4/Debug.java
@@ -9,99 +9,99 @@ import processing.core.PApplet;
 
 public class Debug {
 
-    public PApplet parent;
+    public Board parent;
     public boolean open;
     public int width;
 
 
-    public Debug(PApplet parent, HexTile center){
+    public Debug(Board parent, HexTile center) {
         this.parent = parent;
-        this.width =  (int)parent.textWidth("   Debug Resources   ");
+        this.width = (int) parent.textWidth("   Debug Resources   ");
     }
 
     /**
      * A method for displaying the dropdown menu when closed
      */
-    public void displayClosed(){
-        parent.stroke(0,0,0,0);
+    public void displayClosed() {
+        parent.stroke(0, 0, 0, 0);
         parent.fill(255, 243, 224);
         int y = 10;
         int x = 30;
-        int width = (int)parent.textWidth("   Debug Resources   ");
-        parent.rect(x,y,width,30);
+        int width = (int) parent.textWidth("   Debug Resources   ");
+        parent.rect(x, y, width, 30);
         parent.fill(0);
         parent.textSize(14);
-        parent.text("  Debug  ", (width/2), 30);
+        parent.text("  Debug  ", (width / 2), 30);
     }
 
     /**
      * A method for displaying the dropdown menu when open
      */
-    public void displayOpen(){
+    public void displayOpen() {
         //menu title
-        parent.stroke(0,0,0,50);
-        parent.fill(158,158,158);
+        parent.stroke(0, 0, 0, 50);
+        parent.fill(158, 158, 158);
         int y = 10;
         int x = 30;
-        int width = (int)parent.textWidth("   Debug Resources   ");
+        int width = (int) parent.textWidth("   Debug Resources   ");
         int height = 30;
-        parent.rect(x,y,width,30);
+        parent.rect(x, y, width, 30);
         parent.fill(0);
         parent.textSize(14);
-        parent.text("  Debug  ", (width/2), height);
+        parent.text("  Debug  ", (width / 2), height);
 
         //option 1 = random
-        parent.fill(255,243,224);
+        parent.fill(255, 243, 224);
         int rY = y + 30;
         int rHeight = height + 30;
-        parent.rect(x,rY,width,30);
+        parent.rect(x, rY, width, 30);
         parent.fill(0);
         parent.textSize(14);
         parent.text("  Random  ", 35, rHeight);
 
         //option 2 = classic
-        parent.fill(255,243,224);
+        parent.fill(255, 243, 224);
         int cY = rY + 30;
         int cHeight = rHeight + 30;
-        parent.rect(x,cY,width,30);
+        parent.rect(x, cY, width, 30);
         parent.fill(0);
         parent.textSize(14);
         parent.text("  Classic  ", 35, cHeight);
 
         //option 3 = debug points
-        parent.fill(255,243,224);
+        parent.fill(255, 243, 224);
         int dPY = cY + 30;
         int dPHeight = cHeight + 30;
-        parent.rect(x,dPY,width,30);
+        parent.rect(x, dPY, width, 30);
         parent.fill(0);
         parent.textSize(14);
         parent.text("  Debug Points  ", 35, dPHeight);
 
         //option 4 = debug sides
-        parent.fill(255,243,224);
+        parent.fill(255, 243, 224);
         int dSY = dPY + 30;
         int dSHeight = dPHeight + 30;
-        parent.rect(x,dSY,width,30);
+        parent.rect(x, dSY, width, 30);
         parent.fill(0);
         parent.textSize(14);
         parent.text("  Debug Sides  ", 35, dSHeight);
 
 
         //option 5 = debug resources
-        parent.fill(255,243,224);
-        int dRY = dSY+30;
+        parent.fill(255, 243, 224);
+        int dRY = dSY + 30;
         int dRHeight = dSHeight + 30;
-        parent.rect(x,dRY,width,30);
+        parent.rect(x, dRY, width, 30);
         parent.fill(0);
         parent.textSize(14);
         parent.text("  Debug Resources  ", 35, dRHeight);
 
 
         //option 6 = clear debug
-        parent.fill(255,243,224);
+        parent.fill(255, 243, 224);
         int cLY = dRY + 30;
         int cLHeight = dRHeight + 30;
-        parent.rect(x,cLY,width,30);
+        parent.rect(x, cLY, width, 30);
         parent.fill(0);
         parent.textSize(14);
         parent.text("  Clear Debug ", 35, cLHeight);
@@ -110,15 +110,17 @@ public class Debug {
 
     /**
      * A method for checking to see if the mouse is over the head of the menu
+     *
      * @return
      */
-    public boolean overHead(){
-        return overRect(30,10,width,30);
+    public boolean overHead() {
+        return overRect(30, 10, width, 30);
     }
 
     /**
      * A method for checking which item of the menu is selected
      * returns the menu item id (starting at index 1);
+     *
      * @return
      */
     public int overMenu() {
@@ -152,18 +154,94 @@ public class Debug {
     /**
      * A method for checking a rectangular
      * space for a mouse click
+     *
      * @param x
      * @param y
      * @param width
      * @param height
      * @return
      */
-    boolean overRect(int x, int y, int width, int height)  {
-        if (parent.mouseX >= x && parent.mouseX <= x+width &&
-                parent.mouseY >= y && parent.mouseY <= y+height) {
+    boolean overRect(int x, int y, int width, int height) {
+        if (parent.mouseX >= x && parent.mouseX <= x + width &&
+                parent.mouseY >= y && parent.mouseY <= y + height) {
             return true;
         } else {
             return false;
+        }
+    }
+
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+
+    /**
+     * Handles all the mouse positioning checks for this
+     * menu.
+     */
+    public void mouseDebug() {
+        //open if it's not already
+        if (!isOpen()) {
+            if (this.overHead()) {
+                this.open = true;
+            } else {
+                this.open = false;
+            }
+        }
+        if (isOpen()) {
+            //check to see what option is selected
+            int result = this.overMenu();
+            if (result == 1) {
+                parent.clear();
+                parent.background(0, 188, 212);
+                this.displayClosed();
+                this.open = false;
+                BoardData reInit = new BoardData();
+                parent.center = new HexTile(parent, parent.SCREEN_WIDTH / 2, parent.SCREEN_HEIGHT / 2, 50, reInit, reInit.getResourceTiles()[0], reInit.getTokens()[0]);
+                parent.center.getModel().buildRandomBoard((parent.center));
+            }
+            if (result == 2) {
+                parent.clear();
+                parent.background(0, 188, 212);
+                this.displayClosed();
+                this.open = false;
+                BoardData reInit = new BoardData();
+                parent.center = new HexTile(parent, parent.SCREEN_WIDTH / 2, parent.SCREEN_HEIGHT / 2, 50, reInit, reInit.getResourceTiles()[0], reInit.getTokens()[0]);
+                parent.center.getModel().buildBoard(parent.center);
+            }
+            if (result == 3) {
+                parent.background(0, 188, 212);
+                this.displayClosed();
+                this.open = false;
+                parent.center.getModel().displayBoard();
+                parent.center.getModel().setDisplayMode(1);
+            }
+            if (result == 4) {
+                parent.background(0, 188, 212);
+                this.displayClosed();
+                this.open = false;
+                parent.center.getModel().displayBoard();
+                parent.center.getModel().setDisplayMode(2);
+            }
+            if (result == 5) {
+                parent.background(0, 188, 212);
+                this.displayClosed();
+                this.open = false;
+                parent.center.getModel().displayBoard();
+                parent.center.getModel().setDisplayMode(3);
+            }
+            if (result == 6) {
+                parent.background(0, 188, 212);
+                this.displayClosed();
+                this.open = false;
+                parent.center.getModel().displayBoard();
+                parent.center.getModel().setDisplayMode(0);
+            }
         }
     }
 
